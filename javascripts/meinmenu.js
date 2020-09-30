3090256133f99d29e155e60400c3561384591f15
lgp-base <lgp60383015@gmail.com>
2020-09-30 11:19:49 +0900

    del link

--- javascripts/meinmenu.js
+++ javascripts/meinmenu.js
@@ -2,7 +2,7 @@
 document.write("<a href='./about.html' class='btm' id='no'>about</a> ");
 document.write("<a href='./comic.html' class='btm' id='no'>comic</a> ");
 document.write("<a href='./gallery.html' class='btm' id='no'>gallery</a> ");
-document.write("<a href='./english.html' class='btm'>english</a> ");
+// document.write("<a href='./english.html' class='btm'>english</a> ");
 /* document.write("<a href='./helth.html' class='btm' id='no'>helth</a> ");
 document.write("<a href='./cook.html' class='btm' id='no'>cook</a> "); */
 document.write("<a href='./diary.html' class='btm'>diary</a> ");
--- javascripts/meinmenu_side.js
+++ javascripts/meinmenu_side.js
@@ -3,7 +3,7 @@ document.write("<li><a href='./index.html' name='top'>top</a></li> ");
 document.write("<li><a href='./about.html' id='no'>about</a></li>");
 document.write("<li><a href='./comic.html' id='no'>comic</a></li>");
 document.write("<li><a href='./gallery.html' id='no'>gallery</a></li>");
-document.write("<li><a href='./english.html'>english</a></li>");
+// document.write("<li><a href='./english.html'>english</a></li>");
 /* document.write("<li><a href='./helth.html' id='no'>helth</a></li>");
 document.write("<li><a href='./cook.html' id='no'>cook</a></li>"); */
 document.write("<li><a href='./diary.html'>diary</a></li>");
